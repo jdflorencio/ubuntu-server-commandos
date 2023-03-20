1d857beb7db08b21c22e8c3a40b10fa37e7cc6f8
João Diego Florencio <jdflorencio@gmail.com>
2023-03-20 10:14:11 -0300

    Update criando_usuarios.sh

--- criando_usuarios.sh
+++ criando_usuarios.sh
@@ -15,15 +15,15 @@ groupadd GRP_SEC
 echo "Criando usuarios:"
 
 
-useradd carlos -m -c "Carlos Oliveira" -s /bin/bash -p $(openssl passwd carlos) -g GRP_ADM 
-useradd maria -m -c "Maria Joaquina" -s /bin/bash -p $(openssl passwd maria) -g GRP_ADM
-useradd joao -c "Joao Carlos" -s /bin/bash -p $(openssl passwd joao) -g GRP_ADM
-useradd debora -m -c "Debora Nascimento" -s /bin/bash -p $(openssl passwd debora) -g GRP_VEN 
-useradd sebastiana -m -c "Sebastiana Silva" -s /bin/bash -p $(openssl passwd sebastiana) -g GRP_VEN
-useradd roberto -m -c "Roberto Silvino" -s /bin/bash -p $(openssl passwd roberto) -g GRP_VEN
-useradd josefina -m -c "Josefina Alves" -s /bin/bash -p $(openssl passwd josefina) -g GRP_SEC
-useradd amanda -m -c "Amanda Sousa" -s /bin/bash -p $(openssl passwd amanda) -g GRP_SEC
-useradd rogerio -m -c "Rogerio Silva" -s /bin/bash -p $(openssl passwd rogerio) -g GRP_SEC
+useradd carlos -m -c "Carlos Oliveira" -s /bin/bash -p $(openssl passwd senha123) -g GRP_ADM 
+useradd maria -m -c "Maria Joaquina" -s /bin/bash -p $(openssl passwd senha123) -g GRP_ADM
+useradd joao -c "Joao Carlos" -s /bin/bash -p $(openssl passwd senha123) -g GRP_ADM
+useradd debora -m -c "Debora Nascimento" -s /bin/bash -p $(openssl passwd senha12) -g GRP_VEN 
+useradd sebastiana -m -c "Sebastiana Silva" -s /bin/bash -p $(openssl passwd senha123) -g GRP_VEN
+useradd roberto -m -c "Roberto Silvino" -s /bin/bash -p $(openssl passwd senha123) -g GRP_VEN
+useradd josefina -m -c "Josefina Alves" -s /bin/bash -p $(openssl passwd senha123) -g GRP_SEC
+useradd amanda -m -c "Amanda Sousa" -s /bin/bash -p $(openssl passwd senha123) -g GRP_SEC
+useradd rogerio -m -c "Rogerio Silva" -s /bin/bash -p $(openssl passwd senha123) -g GRP_SEC
 
 echo "criando diretorios"
 chown root:GRP_ADM /adm
